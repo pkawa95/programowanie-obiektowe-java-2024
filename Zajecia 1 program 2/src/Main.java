acca5986fdffda3288caacbf6ccfb1684cb52ac0
Piotr Kawa <piotrkawa95@gmail.com>
2024-04-20 23:25:23 +0200

    Dodawanie poprzednich programów z pierwszych zajęć

---
+++ Zajecia 1 program 2/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main (String[] args)
+    {
+        System.out.println("Witaj w ststemie rezerwacji");
+        System.out.println();
+        displayHotelInformation();
+        System.out.println();
+        displayCompanyInformation();
+    }
+    public static void displayCompanyInformation()
+    {
+        System.out.println("Autorem systemu jest: 78228 ");
+        System.out.println("  Clockwork Java");
+        System.out.println("  ul.Programistów 32");
+        System.out.println("  31-333 Kraków");
+    }
+    public static void displayHotelInformation()
+    {
+        System.out.println(" --- Hotel Clockwork Java --");
+        System.out.println("ul. Programistów 32");
+        System.out.println("31-333 Kraków");
+        System.out.println("tel. 322 444 123");
+    }
+}
+// Indeks 78228
